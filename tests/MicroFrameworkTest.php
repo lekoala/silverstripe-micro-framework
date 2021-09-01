a6716bbec5a48b0ddf990397fd1f254944efca0c
Thomas <thomas@lekoala.be>
2021-09-01 18:43:15 +0200

    add test

---
+++ tests/MicroFrameworkTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LeKoala\MicroFramework\Test;
+
+use SilverStripe\Dev\SapphireTest;
+
+class MicroFrameworkTest extends SapphireTest
+{
+    public function testItRuns()
+    {
+        $this->assertTrue(true);
+    }
+}
